fb76473575445fa3baa3cd12a9d857d14450d146
viranchiB <viranchibhure69@gmail.com>
2025-12-11 15:55:57 +0530

    Initial Commit

---
+++ src/test/java/com/hibernate_mappings_all/HibernateMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate_mappings_all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
